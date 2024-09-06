4564032592a3449c7fe84c8ddea810798da0359f
Quoc Luc Nguyen <quocluc97@gmail.com>
2024-09-06 16:33:52 +0700

    update

--- supabase/functions/_shared/schema.ts
+++ supabase/functions/_shared/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, timestamp } from "npm:drizzle-orm@0.29.1/pg-core";
+import { pgTable, serial, text, timestamp, integer } from "npm:drizzle-orm@0.29.1/pg-core";
 
 export const item = pgTable("item", {
   id: serial("id").primaryKey(),
@@ -9,4 +9,13 @@ export const item = pgTable("item", {
   note: text("note"),
   createdAt: timestamp("created_at"),
   updatedAt: timestamp("updated_at"),
+});
+
+export const user = pgTable("user", {
+  id: serial("id").primaryKey(),
+  created_at: timestamp("created_at"),
+  first_name: text("first_name"),
+  last_name: text("last_name"),
+  username: text("username"),
+  telegram_user_id: integer("telegram_user_id"),
 });
--- supabase/functions/telegram-bot/index.ts
+++ supabase/functions/telegram-bot/index.ts
@@ -2,27 +2,73 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-console.log(`Function "telegram-bot" up and running!`)
+console.log(`Function "telegram-bot" up and running!`);
 
-import { Bot, webhookCallback } from 'https://deno.land/x/grammy@v1.8.3/mod.ts'
+import postgres from "npm:postgres@3.4.3";
+import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
+import { drizzle } from "npm:drizzle-orm@0.29.1/postgres-js/driver";
+import { user } from "../_shared/schema.ts";
+import { eq } from "npm:drizzle-orm@0.29.1/expressions";
 
-const bot = new Bot(Deno.env.get('TELEGRAM_BOT_TOKEN') || '')
+const databaseUrl = Deno.env.get("SUPABASE_DB_URL")!;
+const client = postgres(databaseUrl, { prepare: false });
+const db = drizzle(client, {
+  schema: {
+    user,
+  },
+});
 
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
+const insertUser = async (
+  { first_name, last_name, username, id }: {
+    first_name: string;
+    last_name?: string;
+    username?: string;
+    id: number;
+  },
+) => {
+  await db.insert(user).values({
+    first_name,
+    last_name,
+    username,
+    telegram_user_id: id,
+  });
+};
 
-bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
+const findUserById = async (id: number) => {
+  return await db.select().from(user).where(eq(user.telegram_user_id, id))
+    .limit(1);
+};
 
-const handleUpdate = webhookCallback(bot, 'std/http')
+/**
+ * Bot instance
+ */
+const bot = new Bot(
+  Deno.env.get("TELEGRAM_BOT_TOKEN") ||
+    "5255532380:AAFYA3U2gX3-KVCrc3vG6S5mRrWGFjTJQ4c",
+);
+
+bot.command("start", async (ctx) => {
+  if (ctx?.message?.from) {
+    const users = await findUserById(ctx.message.from.id);
+    if (users.length > 0) {
+      ctx.reply("Welcome back!");
+      ctx.reply("👋");
+      return;
+    } else {
+      await insertUser(ctx.message.from);
+    }
+  }
+  ctx.reply("Welcome! Up and running.");
+});
+
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+const handleUpdate = webhookCallback(bot, "std/http");
 
 Deno.serve(async (req) => {
   try {
-    const url = new URL(req.url)
-    if (url.searchParams.get('secret') !== Deno.env.get('FUNCTION_SECRET')) {
-      return new Response('not allowed', { status: 405 })
-    }
-
-    return await handleUpdate(req)
+    return await handleUpdate(req);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})
+});
