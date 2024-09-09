849a9fc1ed57097bbdebc22836b1f5a65594bb0c
Quoc Luc Nguyen <quocluc97@gmail.com>
2024-09-09 15:19:14 +0700

    update teleram bot

--- supabase/functions/telegram-bot/index.ts
+++ supabase/functions/telegram-bot/index.ts
@@ -1,10 +1,13 @@
-import "jsr:@std/dotenv/load";
 import { webhookCallback } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
-import { user } from "../_shared/schema.ts";
-import { eq } from "npm:drizzle-orm@0.29.1/expressions";
+import "jsr:@std/dotenv/load";
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import db from "../_shared/db.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
 import bot from "../_shared/bot.ts";
+
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL") ?? "",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+);
 
 const insertUser = async (
   { first_name, last_name, username, id }: {
@@ -14,7 +17,7 @@ const insertUser = async (
     id: number;
   },
 ) => {
-  await db.insert(user).values({
+  await supabase.from("user").insert({
     first_name,
     last_name,
     username,
@@ -23,16 +26,14 @@ const insertUser = async (
 };
 
 const findUserById = async (id: number) => {
-  return await db.select().from(user).where(eq(user.telegram_user_id, id))
-    .limit(1);
+  return await supabase.from("user").select("*").eq("telegram_user_id", id);
 };
-
-
 
 bot.command("start", async (ctx) => {
   if (ctx?.message?.from) {
     const users = await findUserById(ctx.message.from.id);
-    if (users.length > 0) {
+
+    if (users?.data?.length ?? 0 > 0) {
       ctx.reply("Welcome back!");
       ctx.reply("👋");
       return;
